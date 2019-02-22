4b5e74a8c744a50c1767ab3fe94e1bd31dab6517
hkg5600 <46782752+hkg5600@users.noreply.github.com>
2019-02-23 00:33:32 +0900

    Update ServerUI.java

--- ServerUI.java
+++ ServerUI.java
@@ -11,18 +11,18 @@ public class ServerUI {
 	 */
 	//private static final long serialVersionUID = 1L;
 	
-	Socket c_socket = null;
+	Socket c_socket = null; //소켓 객체 생성
 	
-	public int first_con() {
+	public int first_con() { //처음 소켓 연결에 사용할 메소드 반환값은 성공여부이다
 		try{
-			String data = "Hi";
-			c_socket = new Socket("localhost", 1212);
+			String data = "Hi"; //소켓에 보낼 메세지
+			c_socket = new Socket("localhost", 1212); //ip주소와 포트번호
 			//ta.append("connected!");
-			OutputStream output_data = c_socket.getOutputStream();
-			output_data.write(data.getBytes());
-			InputStream input_data = c_socket.getInputStream();
-			byte[] receiveBuffer = new byte[100];
-			input_data.read(receiveBuffer);
+			OutputStream output_data = c_socket.getOutputStream(); //데이터 전송에 사용할 객체
+			output_data.write(data.getBytes()); //데이터 전송 data변수에 저장된다
+			InputStream input_data = c_socket.getInputStream(); //데이터 수신에 사용할 객체
+			byte[] receiveBuffer = new byte[100]; //바이트 형식으로 리시브버퍼 생성
+			input_data.read(receiveBuffer); //리시브버퍼에 데이터를 받는다
 			System.out.println("message from server : " + new String(receiveBuffer));
 			if (receiveBuffer != null) {
 				return 1;
@@ -34,12 +34,12 @@ public class ServerUI {
 		}
 		return 0;
 	}
-	public void sendServer(String data) {
-		try {
-		OutputStream output_data = c_socket.getOutputStream();
-		output_data.write(data.getBytes());
+	public void sendServer(String data) { //연결이 성공적으로 이루어진 후에 데이터 전송에 사용될 메소드 
+		try {							  //인자 값은 전송될 데이터
+		OutputStream output_data = c_socket.getOutputStream(); //전송 객체 생성
+		output_data.write(data.getBytes()); //전송
 		} catch(IOException e) {
-			e.printStackTrace();
+			e.printStackTrace(); //에러 출력
 		}
 	}
 
